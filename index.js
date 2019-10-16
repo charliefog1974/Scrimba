2fc195aedf307aac5abbe4414d1725408bde20e6
Vince Lollino <charlie_fog@comcast.net>
2019-10-16 15:07:47 -0500

    added to VSC

---
+++ index.js
@@ -0,0 +1,27 @@
+//- parseInt function
+
+// function convertToInteger(str) {
+//     return parseInt(str);
+// }
+
+
+
+// console.log(convertToInteger("800"));
+
+// parseInt w/Radix uses a base, base 10 is the default, base 2 is binary
+
+    // function convertToInteger(str) {
+    //     return parseInt(str,2)
+    // }
+    // console.log(convertToInteger("100000"));
+    
+// ternary - concition ? statement-if-true : statement-if-false
+
+    function checkEqual (a,b,) {
+        // return a === b ? true : false;
+        // will never do it this way can use:
+        return a === b;
+        
+    }
+      
+   console.log(checkEqual(.25,1/2));
